adf0e9ae2f9700fe88154a935002811fb5c249ef
essioui <salahessioui@gmail.com>
2024-05-08 17:32:27 +0100

    other_method_find

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-char = ""
 for alphabet in range(97, 123):
-    char += "{:c}".format(alphabet) # you can use .format(alph),end=""
-print(char)
+    print("{:c}".format(alphabet), end="")
