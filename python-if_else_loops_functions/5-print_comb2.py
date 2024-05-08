0792ff71190fc5586ee7a5127ef1850980d68906
essioui <salahessioui@gmail.com>
2024-05-08 22:15:58 +0100

    print numbers with two digits

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-for i in range(99):
-    if i < 10:
-        print("0{} ,".format(i), end="")
-    elif i > 9:
-        print("{} ,".format(i), end="")
+for i in range(100):
+    if i < 99:
+        print("{:02d} ,".format(i), end="")
+    else:
+        print(i)
         
-
-
-
