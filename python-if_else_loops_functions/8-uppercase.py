2fd2753132a3a84e47a4cd62ba2791e58d6e4562
essioui <salahessioui@gmail.com>
2024-05-09 12:22:52 +0100

    upper

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,8 @@
 def uppercase(str):
     for i in str:
         if ord(i) >= ord('a') and ord(i) <= ord('z'):
-            # ord(i) transfer letter to number ASCII and ord('a') = 97 & ord('z') = 123
+            # ord(i) transfer letter to number
+            # and ord('a') = 97 & ord('z') = 123
             i = chr(ord(i) - 32)
             # chr() function transfer number to letter & -32 for uppercase
         print("{}".format(i), end="")
