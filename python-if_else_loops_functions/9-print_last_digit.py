8727ad923d15346f484363ca1605c4ae861f5ade
essioui <salahessioui@gmail.com>
2024-05-09 12:41:13 +0100

    last_digits

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
+    if number < 0:
+        number *= -1
+    else: 
+        number = number
     print(number % 10, end="")
     return (number % 10)
