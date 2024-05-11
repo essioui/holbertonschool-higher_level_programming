9cd4e94c2330b7be5fac74817d617565f138d4d0
essioui <salahessioui@gmail.com>
2024-05-11 13:53:17 +0100

    reverse order

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+alphabet =""
 for i in range(122, 64, -1):
     if i % 2 == 0:
-        print(chr(i), end="")
+        alphabet += chr(i)
     else :
-        print(chr(i).upper(), end="")
+        alphabet += chr(i).upper()
+print(alphabet, end="")
+
