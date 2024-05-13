aa817cf5add194c22bca90cc9a9be239d707c981
essioui <salahessioui@gmail.com>
2024-05-13 23:09:31 +0100

    fix source code 3rd

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -4,7 +4,7 @@ def delete_at(my_list=[], idx=0):
         return (my_list)
     else:
         del(my_list[idx])
-        return (my_list)
+    return (my_list)
     
 my_list = [1, 2, 3, 4, 5]
 idx = 1
