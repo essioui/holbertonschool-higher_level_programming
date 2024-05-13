f99d2c85bc779f55e3f49fea1ede5cfefbf80fe2
essioui <salahessioui@gmail.com>
2024-05-13 18:36:48 +0100

    returns multiple

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     length = len(sentence)
     if not sentence:
-        return f"Length: {length} - First character: {None}"
+        return (length, None)
     else:
         return (length, sentence[0])
