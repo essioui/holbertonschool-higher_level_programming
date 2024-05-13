29f5afb4765437f2ce9769cb0a22feafec40d603
essioui <salahessioui@gmail.com>
2024-05-13 15:23:22 +0100

    print matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,18 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
-            print("{:d}".format(j), end=" ")
+            print("{:d} ".format(j), end="")
         print()
+         
 
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
