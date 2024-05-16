bf07b75109b9158670cd3918e75cbea44e3550d6
essioui <salahessioui@gmail.com>
2024-05-16 14:41:08 +0100

    print x from list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     counter = 0
-    try:
-        for i in my_list:
-            if counter < x:
-                print("{}".format(my_list[counter]), end="")
-                counter += 1
-        print()
-    except TypeError:
-        pass
-    finally:
-        return (counter)
+
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            counter += 1
+        except IndexError:
+            break
+    print()
+    return counter if counter <= x else x
