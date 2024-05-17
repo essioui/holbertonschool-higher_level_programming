64ea0cad7d011f8e8434594c89c418c3ed23e3b9
essioui <salahessioui@gmail.com>
2024-05-17 09:40:17 +0100

    script0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     counter = 0
-
     try:
         for i in range(x):
             print(my_list[i], end="")
             counter += 1
     except IndexError:
         pass
-    if counter > 0:
+    finally:
         print() 
     return counter
