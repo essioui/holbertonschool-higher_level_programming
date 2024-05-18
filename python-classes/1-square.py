656b7c3de4ebf3d335b5c320a4b66401a081a3a0
essioui <salahessioui@gmail.com>
2024-05-18 23:46:19 +0100

    square with size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+Define class square
+"""
 class Square:
+    """
+    Represent a square
+    """
     def __init__(self, size):
+        """
+        intialize a new square
+        """
         self.__size = size
