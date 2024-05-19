d64e4341dc5775a87772d06608452cfffa8bbca4
essioui <salahessioui@gmail.com>
2024-05-19 15:34:19 +0100

    coordinate of a square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,44 +1,46 @@
+#!/usr/bin/python3
 class Square:
+    """Square Class
+
+    A Square Class
+
+    """
+        
     def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new square."""
         self.size = size
         self.position = position
 
-    @property
     def size(self):
-        return self._size
+        return self.__size
 
-    @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        self._size = value
+        self.__size = value
 
-    @property
     def position(self):
-        return self._position
+        return self.__position
 
-    @position.setter
     def position(self, value):
-        if (
-            not isinstance(value, tuple)
-            or len(value) != 2
-            or not all(isinstance(num, int) for num in value)
-            or not all(num >= 0 for num in value)
+        if not (
+            isinstance(value, tuple)
+            and len(value) == 2
+            and all(isinstance(i, int) and i >= 0 for i in value)
         ):
             raise TypeError("position must be a tuple of 2 positive integers")
-        self._position = value
+        self.position = value
+
+    def area(self):
+        return self.__size * self.__size
 
     def my_print(self):
         if self.size == 0:
-            print("")
-            return
-
-        # Print the vertical offset
+            print()
+            return None
         for _ in range(self.position[1]):
-            print("")
-
-        # Print the square rows with the horizontal offset
+            print()
         for _ in range(self.size):
             print(" " * self.position[0] + "#" * self.size)
