1163a8c061b831ba23fe11377970ed7fd5f6adbd
essioui <salahessioui@gmail.com>
2024-05-19 01:12:33 +0100

    square print #

---
+++ python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+
+    def size(self):
+        return (self.__size)
+
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+            return
+        for i in range(0, self.__size):
+            print("#" * self.__size)
+
+#!/usr/bin/python3
+Square = __import__('5-square').Square
+
+my_square = Square(3)
+my_square.my_print()
+
+print("--")
+
+my_square.size = 10
+my_square.my_print()
+
+print("--")
+
+my_square.size = 0
+my_square.my_print()
+
+print("--")
+
