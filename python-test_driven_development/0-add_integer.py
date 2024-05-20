80af517dfc40ecd3066c09f9df5a5c8132dc4011
essioui <salahessioui@gmail.com>
2024-05-20 15:28:45 +0100

    my first test module

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,6 +2,8 @@
 """
 Define addition integer function
 """
+
+
 def add_integer(a, b=98):
     """
     Return the sum of a and b
@@ -10,8 +12,14 @@ def add_integer(a, b=98):
     if type a or b is not integer and not float
     return int a + int b
     """
-    if (not isinstance(a, int) and not isinstance(a, float)):
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if (not isinstance(b, int) and not isinstance(b, float)):
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-    return (int(a) + int(b))
+    return int(a) + int(b)
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testfile("tests/0-add_integer.txt")
