e0f6018ea68228f04771151024bb7d8bdfd6673e
essioui <salahessioui@gmail.com>
2024-05-21 13:12:00 +0100

    say_my_name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -18,7 +18,11 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("last_name must be a string")
 
     if last_name:
-        print(f"My name is {first_name}{last_name}")
+        print(f"My name is {first_name} {last_name}")
     else:
         print(f"My name is {first_name}")
 
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testfile("tests/3-say_my_name.txt")
