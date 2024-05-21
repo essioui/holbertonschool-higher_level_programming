aa0ff73176e245a599f87c5b6f3391cbdb8d5ddf
essioui <salahessioui@gmail.com>
2024-05-21 13:15:30 +0100

    print my name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -20,7 +20,7 @@ def say_my_name(first_name, last_name=""):
     if last_name:
         print(f"My name is {first_name} {last_name}")
     else:
-        print(f"My name is {first_name}")
+        print("My name is {} ".format(first_name))
 
 if __name__ == "__main__":
     import doctest
