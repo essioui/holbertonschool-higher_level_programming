0da4641c7dc5a349dc3b1b141449f24731bdc3a0
essioui <salahessioui@gmail.com>
2024-05-21 01:10:33 +0100

    say my name is:

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,17 @@
-#!/usr/in/python3
+#!/usr/bin/python3
+"""
+Module print my first name and last name
+"""
+
+
 def say_my_name(first_name, last_name):
+    """
+    Print my name
+    Args: frist_name: must be a string
+          frist_name: must be a string
+    Return:
+    My name is first_name last_name
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
