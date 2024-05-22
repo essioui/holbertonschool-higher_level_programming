a8ae5e154c48b20b634eb053c1e6c6610912971b
essioui <salahessioui@gmail.com>
2024-05-22 13:36:47 +0100

    print width height of rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -38,15 +38,3 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
-
-
-#!/usr/bin/python3
-Rectangle = __import__('1-rectangle').Rectangle
-
-my_rectangle = Rectangle(-2, 4)
-print(my_rectangle.__dict__)
-
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle.__dict__)
