c091d91bc11ca54c624c20eda096ccead9153984
essioui <salahessioui@gmail.com>
2024-05-22 13:27:11 +0100

    print width height of rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -6,33 +6,32 @@ Definie Rectangle class
 
 class Rectangle:
     """
-    Class Rectangle
-    Args:
-    width (int): must be a integer positif
-    height (int): must be a integer positif
-    Raises:
-    TypeError: width must be an integer
-    ValueError: width must be >= 0
-    TypeError: height must be an integer
-    ValueError: height must be >= 0
+    Represent new rectangle.
     """
+
     def __init__(self, width=0, height=0):
+        """
+        new Rectangle
+        Args:
+        width (int): must be a integer positif
+        height (int): must be a integer positif
+        """
         self.width = width
         self.height = height
 
     def width(self):
         return (self.__width)
-    
+
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-        
+
     def height(self):
         return (self.__height)
-    
+
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
