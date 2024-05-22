92ba3e816ee3e2397e6140c0478ba373c07c94dc
essioui <salahessioui@gmail.com>
2024-05-22 02:06:45 +0100

    print width and height

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -17,8 +17,8 @@ class Rectangle:
     ValueError: height must be >= 0
     """
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     def width(self):
         return (self.width)
