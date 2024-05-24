c0e943fb72e92c2910cca840f2ac02fdb706b6e7
essioui <salahessioui@gmail.com>
2024-05-24 21:44:08 +0100

    use try except

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -25,11 +25,14 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self._width = value
+        try:
+            if not isinstance(value, int):
+                raise TypeError("height must be an integer")
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            self._height = value
+        except Exception as e:
+            print("[{}] {}".format(e.__class__.__name__, e))
 
     @property
     def height(self):
@@ -37,11 +40,14 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self._height = value
+        try:
+            if not isinstance(value, int):
+                raise TypeError("height must be an integer")
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            self._height = value
+        except Exception as e:
+            print("[{}] {}".format(e.__class__.__name__, e))
 
     def area(self):
         return self._width * self._height
