7c06dbc657501d70febc18ce6cae2c1e9d5efeae
essioui <salahessioui@gmail.com>
2024-05-26 00:58:54 +0100

    indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
 """
-Module prints a text with 2 new lines.
+This module provides a function `text_indentation.
 """
 
 
 def text_indentation(text):
     """
-    text_indentation prints text with two lines after charcters
+    Prints a text with 2 new lines after each of these characters: ., ? and :
+
     Args:
-    text (str): the type of text is string
+        text (str): The text to be printed
+
     Raises:
-    TypeError: text must be a string
-    Return:
-    print text line per line
+        TypeError: If text is not a string
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    text = text.replace(".", ".").replace("?", ".").replace(":", ".")
-    text = text.split(".")
-    for line in text:
-        result = "".join(line.strip())
-        result = result.strip()
-        print(result)
+
+    special_characters = {'.', '?', ':'}
+    i = 0
+    while i in range(len(text)):
+        if text[i] in special_characters:
+            print(f"{text[i]}\n\n", end="")
+            i += 1
+            while i < len(text) and text[i] == ' ':
+                i += 1
+        else:
+            print(text[i], end="")
+            i += 1
 
 
 if __name__ == "__main__":
     import doctest
-
     doctest.testfile("tests/5-text_indentation.txt")
