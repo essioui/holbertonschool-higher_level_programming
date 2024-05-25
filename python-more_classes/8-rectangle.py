e78a06e57c959ed98ea45eed77c1cfc35b46e8df
essioui <salahessioui@gmail.com>
2024-05-26 00:46:19 +0100

    compare rectangle

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -122,7 +122,7 @@ class Rectangle:
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
-    
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """
