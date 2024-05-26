aa365f8d8748f06e3dac53b01ba8c659c5f7f49e
essioui <salahessioui@gmail.com>
2024-05-26 15:25:12 +0100

    Improve Geometry

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -6,7 +6,5 @@ class BaseGeometry:
     """empty class"""
 
     def area(self):
-        """ Improve Geometry"""
-        def area(self):
-            """that raises an Exception with the message"""
-            raise Exception("area() is not implemented")
+        """that raises an Exception with the message"""
+        raise Exception("area() is not implemented")
