a26a4be2191b8a7624c525895b4dfd869f5f5230
essioui <salahessioui@gmail.com>
2024-05-27 00:33:41 +0100

    duck_typing 1

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -25,9 +25,8 @@ class Circle(Shape):
     """
 
     def __init__(self, radius):
-        if radius <= 0:
-            raise ValueError("Radius must be a positive number")
-        self.radius = radius
+        if type(radius) is int:
+            self.radius = radius
 
     def area(self):
         return math.pi * self.radius ** 2
