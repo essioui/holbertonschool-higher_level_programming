c49ce54cb498cb3653a34dd2531331e5b6175698
essioui <salahessioui@gmail.com>
2024-05-26 18:29:42 +0100

    Integer validaton

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -23,7 +23,8 @@ class BaseGeometry:
         """
 
         if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value < 0:
-            raise ValueError(f"{name} must be greater 0")
+            raise ValueError("{} must be greater 0".format(name))
+
 
