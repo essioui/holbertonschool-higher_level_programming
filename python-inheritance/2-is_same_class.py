f00ba2b3b2ffafe729f76601fc1e496ec442cbf1
essioui <salahessioui@gmail.com>
2024-05-26 14:55:40 +0100

    Exact same object

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a Class."""
+
+
+def is_same_class(obj, a_class):
+    """
+    returns True if the object is exactly an instance of the specified class 
+    """
+    if type(obj) == a_class:
+        return True
+    return False
