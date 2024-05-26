72ea7f829bab6539532f443056d4b8c213b7a674
essioui <salahessioui@gmail.com>
2024-05-26 14:41:13 +0100

    print my list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Define class MyList
+"""
+
+
+class MyList:
+    def print_sorted(self):
+        """
+        prints the list, but sorted (ascending sort)
+        """
+        print(sorted(self))
