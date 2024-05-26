9dbbc8af9a5072298ec3a49ac9de17e723018f79
essioui <salahessioui@gmail.com>
2024-05-27 00:05:12 +0100

    The abstract method in the base class doesn’t have a body

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,28 +1,25 @@
-#!/usr/bin/python3
-
 from abc import ABC, abstractmethod
 
 class Animal(ABC):
     @abstractmethod
-    def make_sound(self):
+    def sound(self):
         pass
 
 class Dog(Animal):
-    def make_sound(self):
-        return "Bark!"
+    def sound(self):
+        return "Bark"
 
 class Cat(Animal):
-    def make_sound(self):
-        return "Meow!"
+    def sound(self):
+        return "Meow"
 
 if __name__ == "__main__":
-
     bobby = Dog()
     garfield = Cat()
 
-    print(bobby.make_sound())
-    print(garfield.make_sound())
+    print(bobby.sound())
+    print(garfield.sound())
 
-# The following line will raise a TypeError because Animal is abstract
-# animal = Animal()
-# print(animal.make_sound())
+    # Trying to instantiate an Animal object will raise a TypeError
+    # animal = Animal()
+    # print(animal.sound())
