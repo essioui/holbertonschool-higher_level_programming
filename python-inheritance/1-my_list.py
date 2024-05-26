ffea41891b3db656f657fd87c80ece6ee6363dc1
essioui <salahessioui@gmail.com>
2024-05-26 14:43:35 +0100

    print my list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,6 +5,9 @@ Define class MyList
 
 
 class MyList:
+    """
+    MyList inherits from list
+    """
     def print_sorted(self):
         """
         prints the list, but sorted (ascending sort)
