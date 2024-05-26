9ac09f8099f89a67b2edc049a0f816414f39e2b0
essioui <salahessioui@gmail.com>
2024-05-26 15:15:56 +0100

    Only sub class of

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,7 +5,7 @@
 def inherits_from(obj, a_class):
     """
      returns True if the object is an instance of a class that inherited \
-        (directly or indirectly) from the specified class 
+        (directly or indirectly) from the specified class
             Args:
         obj (any): The object to check.
         a_class (type): The class to match the type of obj to.
