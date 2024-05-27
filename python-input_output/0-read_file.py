3774fa37942baf27cd9803389a75d7276f9aee42
essioui <salahessioui@gmail.com>
2024-05-27 15:49:09 +0100

    read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Define module"""
 def read_file(filename=""):
     """
     function that reads a text file (UTF8) and prints it to stdout
@@ -8,14 +9,9 @@ def read_file(filename=""):
         Args:
         with: closed file automatically when finish and have error \
               dont using exception (__enter__ and __exit__)
-        encoding: default is utf-8
+        encoding: file open and read in mode utf-8
 
         as file: file object take contain of file for read.
         """
         print(file.read(), end="")
         """read(): mode reading"""
-
-#!/usr/bin/python3
-read_file = __import__('0-read_file').read_file
-
-read_file("tests/my_file_0.txt")
