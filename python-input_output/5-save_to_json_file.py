a42494fd94b3b7dac7158adcf0ca4e904157a3b3
essioui <salahessioui@gmail.com>
2024-05-27 18:11:24 +0100

    save to json file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -10,11 +10,5 @@ def save_to_json_file(my_obj, filename):
     my_obj: the object to be savee
     filename: the name of the json file
     """
-
-    try:
-        with open(filename, 'w') as json_file:
-            json.dump(my_obj, json_file)
-    except TypeError as e:
-        raise TypeError(f"Object {my_obj} cannot be serialized to JSON") from e
-    except PermissionError as e:
-        raise PermissionError(f"No permission to write to file {filename}") from e
+    with open(filename, "w") as file:
+        json.dump(my_obj, file)
