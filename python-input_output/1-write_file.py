fd2964ffc2da43e3c0b7a518fea40985f1142215
essioui <salahessioui@gmail.com>
2024-05-27 16:11:16 +0100

    write file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def write_file(filename="", text=""):
     """Define module"""
-    with open(filename, 'w', encoding='utf-8') as file:
+    with open(filename, "w", encoding="utf-8") as file:
         """
         filename: the file what we open it
         w: write mode
         encoding: we write file with coding utf-8
         """
         file.write(text)
-        return (len(text))
+        return len(text)
