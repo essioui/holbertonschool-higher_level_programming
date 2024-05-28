6c4588b4880572688396a0e07364bc3014ef1683
essioui <salahessioui@gmail.com>
2024-05-28 14:48:05 +0100

    add load save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,24 +4,19 @@ import sys
 import os.path
 
 
-if __name__ == '__main()__':
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_filen
+import sys
+import os
 
 
-def main():
-    """الدالة الرئيسية للتعامل مع إضافة المعاملات إلى ملف JSON."""
-    data = []
-    args = sys.argv[1:]
-    
-    if os.path.isfile("add_item.json"):
-        data = load_from_json_file("add_item.json")
-    
-    # تأكد من أن البيانات هي قائمة لإضافة المعاملات الجديدة
-    if not isinstance(data, list):
-        raise ValueError("البيانات الموجودة في JSON ليست قائمة.")
-    
-    data.extend(args)
-    save_to_json_file(data, "add_item.json")
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
+filename = 'add_item.json'
 
+if os.path.exists(filename):
+    items = load_from_json_file(filename)
+else:
+    items = []
+
+items.extend(sys.argv[1:])
+save_to_json_file(items, filename)
