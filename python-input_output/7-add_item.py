d146a63654597de05a9ee497c9f662b443a64463
essioui <salahessioui@gmail.com>
2024-05-28 14:46:04 +0100

    add load save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
 """Define module"""
 import sys
-from os.path import exists
+import os.path
 
 
 if __name__ == '__main()__':
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = __import__('6-load_from_json_file').load_from_json_filen
 
-    filename = "add_item.json"
+
+def main():
+    """الدالة الرئيسية للتعامل مع إضافة المعاملات إلى ملف JSON."""
+    data = []
+    args = sys.argv[1:]
+    
+    if os.path.isfile("add_item.json"):
+        data = load_from_json_file("add_item.json")
+    
+    # تأكد من أن البيانات هي قائمة لإضافة المعاملات الجديدة
+    if not isinstance(data, list):
+        raise ValueError("البيانات الموجودة في JSON ليست قائمة.")
+    
+    data.extend(args)
+    save_to_json_file(data, "add_item.json")
 
 
-    if exists(filename):
-        """Load list from the file if it exists, otherwise create a new list"""
-        items = load_from_json_file(filename)
-    else:
-        items = []
-
-    items.extend(sys.argv[1:])
-    """Add all arguments to the list"""
-
-    save_to_json_file(items, filename)
-    """save arguments in the file"""
