439af1e244addde50cf46aa694f7775ce2116e46
essioui <salahessioui@gmail.com>
2024-05-28 01:03:30 +0100

    add, load, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 """Define module"""
 import sys
+from os.path import exists
 
 
 if __name__ == '__main()__':
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = __import__('6-load_from_json_file').load_from_json_filen
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
+    filename = "add_item.json"
+
+
+    if exists(filename):
+        """Load list from the file if it exists, otherwise create a new list"""
+        items = load_from_json_file(filename)
+    else:
         items = []
+
     items.extend(sys.argv[1:])
     """Add all arguments to the list"""
-    save_to_json_file(items, "add_items.json")
-    """Save the updated list back to the file"""
+
+    save_to_json_file(items, filename)
+    """save arguments in the file"""
