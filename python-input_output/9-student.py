4335f43be7bfd991cf024576b8956bfd2915ba2f
essioui <salahessioui@gmail.com>
2024-05-28 15:13:46 +0100

    student of dictionary

---
+++ python-input_output/9-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Define student class"""
+
+
+class Student:
+    """
+    class present a student
+    Attributes:
+    first_name
+    last_name
+    age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return (self.__dict__)
