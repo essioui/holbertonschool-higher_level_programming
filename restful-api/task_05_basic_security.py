8c4a534e4ab5de1f08f7c9034e7c312298d07b9d
essioui <salahessioui@gmail.com>
2024-06-02 02:51:19 +0100

    security

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -3,12 +3,7 @@
 from flask import Flask, jsonify, request
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_httpauth import HTTPBasicAuth
-from flask_jwt_extended import (
-    JWTManager,
-    create_access_token,
-    jwt_required,
-    get_jwt_identity,
-)
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key_here"
