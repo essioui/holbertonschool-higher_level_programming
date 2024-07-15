f695137e0474a9618b8118c0fefb11d03af7b6e0
essioui <salahessioui@gmail.com>
2024-07-15 19:44:44 +0100

    Loops and Conditions in Flask

---
+++ python-server_side_rendering/task_02_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+@app.route('/items')
+def items():
+    
+    with open('items_test.json', 'r', encoding='utf-8') as json_file:
+        data = json.load(json_file)
+        items = data['items']
+    
+    
+    return render_template('items.html', items=items)
+
+if __name__ == '__main__':
+    app.run(debug=True)
