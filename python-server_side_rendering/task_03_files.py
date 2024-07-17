a5ceda15e44cd39eb4d4428ded2441c056015367
essioui <salahessioui@gmail.com>
2024-07-17 18:38:03 +0100

    source

--- python-server_side_rendering/task_03_files.py
+++ python-server_side_rendering/task_03_files.py
@@ -1,28 +1,11 @@
 #!/usr/bin/python3
 
 from flask import Flask, render_template, request
-import json, csv
+import json
+import csv
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
-@app.route('/items')
-def items():
-    with open('items.json') as json_file:
-        data = json.load(json_file)
-    item_list = data.get('items', [])
-    return render_template('items.html', items=item_list)
 
 def read_json(file_path):
     try:
@@ -33,6 +16,7 @@ def read_json(file_path):
         print(f"Error reading JSON file: {e}")
         return []
 
+
 def read_csv(file_path):
     try:
         with open(file_path, newline='') as csvfile:
@@ -42,19 +26,25 @@ def read_csv(file_path):
         print(f"Error reading CSV file: {e}")
         return []
 
+
 @app.route('/products')
 def products():
+    
     source = request.args.get('source')
     product_id = request.args.get('id', type=int)
+
 
     if source not in ['json', 'csv']:
         return render_template('product_display.html', error="Wrong source."), 400
 
     products = []
+
+
     if source == 'json':
         products = read_json('products.json')
     elif source == 'csv':
         products = read_csv('products.csv')
+
 
     if product_id is not None:
         products = [product for product in products if int(product['id']) == product_id]
@@ -64,4 +54,4 @@ def products():
     return render_template('product_display.html', products=products)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
