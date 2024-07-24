4c2edade9184d27bf0fb68f3728679f7b9ab1f54
essioui <salahessioui@gmail.com>
2024-07-24 16:46:16 +0100

    Add `.red` class

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,8 @@
+const redHeader = document.querySelector('#red_header');
+
+redHeader.addEventListener('click', () => {
+
+    const header = document.querySelector('header');
+
+    header.classList.add('red');
+});
