a424aa2751859bba9f213f485a77c59002ad069a
essioui <salahessioui@gmail.com>
2024-07-26 16:03:20 +0100

    translation of “hello”

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const helloElement = document.querySelector('#hello');
+  
+    
+    fetch('https://hellosalut.stefanbohacek.dev/?lang=fr')
+
+      .then(response => response.json())
+
+      .then(data => {
+        
+        helloElement.textContent = data.hello;
+
+      })
+
+      .catch(error => {
+
+        console.error('Error fetching data:', error);
+
+      });
+
+  });
